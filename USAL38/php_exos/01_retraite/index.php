135017cfe1948ece58732a6d3c4025489e22b68a
MDevoldere <mdevoldere@users.noreply.github.com>
2020-12-04 15:17:52 +0100

    EXOS 01 retraite

---
+++ USAL38/php_exos/01_retraite/index.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+L'âge de la retraite est fixé à 55 ans
+
+SI l'âge fourni ($age) est supérieur à l'âge de la retraite ($retraite)
+    alors
+    afficher (echo) "vous êtes à la retraite depuis X années"
+
+SINON SI l'âge fourni ($age) est inférieur à l'âge de la retraite ($retraite)
+    alors   
+    afficher (echo) "Il vous reste X années  avant la retraite"
+
+SINON 
+    alors
+    afficher (echo) "La retraite c'est cette année"
+
+
+*/
+
+$retraite = 55;
+
+$age = 60;
+
+
+if() {
+
+    $difference = ???
+    echo "vous êtes à la retraite depuis $difference années";
+}
+
