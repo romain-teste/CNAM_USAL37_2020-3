957d78fd13c34b6fcc99cae72dcfdb7170a19fa0
MDevoldere <mdevoldere@users.noreply.github.com>
2020-12-04 16:57:32 +0100

    exo retraite 2

--- USAL38/php_exos/01_retraite/index.php
+++ USAL38/php_exos/01_retraite/index.php
@@ -2,29 +2,40 @@
 /*
 L'âge de la retraite est fixé à 55 ans
 
-SI l'âge fourni ($age) est supérieur à l'âge de la retraite ($retraite)
-    alors
+SI l'âge fourni est supérieur à l'âge de la retraite
     afficher (echo) "vous êtes à la retraite depuis X années"
 
 SINON SI l'âge fourni ($age) est inférieur à l'âge de la retraite ($retraite)
-    alors   
-    afficher (echo) "Il vous reste X années  avant la retraite"
-
+        SI l'âge est inférieur à 0 
+            afficher "un peu jeune pour penser à la retraite !"
+        SINON SI l'âge est inférieur à 18
+            afficher "vous pas être né"
+        SINON 
+            afficher (echo) "Il vous reste X années  avant la retraite"
 SINON 
     alors
     afficher (echo) "La retraite c'est cette année"
-
 
 */
 
 $retraite = 55;
 
-$age = 60;
+$age = -12;
 
 
-if() {
 
-    $difference = ???
-    echo "vous êtes à la retraite depuis $difference années";
+if($age > $retraite) { 
+    $difference = ($age - $retraite);
+    echo "Vous êtes à la retraite depuis $difference années";
 }
+else if ($age < $retraite) {
+
+    $difference = ($retraite - $age);
+    echo "Il vous reste $difference années  avant la retraite";
+}
+else {
+    echo "La retraite c'est cette année";
+}
+
+echo "\n FIN";
 
---
+++ USAL38/php_exos/01_retraite/test.php
@@ -0,0 +1,26 @@
+<?php 
+
+$difference = 40;
+
+
+echo "Vous 'êtes' à la \"retraite\" depuis $difference années";
+
+echo "\n";
+
+echo 'Vous \'êtes\' à la "retraite" depuis ' . $difference . ' années';
+
+echo "\n\n";
+
+echo "<p id=\"1\" class=\"uneclasse\">du texte</p>";
+
+echo "\n";
+
+echo '<p id="1" class="uneclasse">du texte</p>';
+
+echo "\n\n";
+
+echo "SELECT * FROM users WHERE username='mike';"; 
+
+echo "\n"; 
+
+echo 'SELECT * FROM users WHERE username=\'mike\';'; 
