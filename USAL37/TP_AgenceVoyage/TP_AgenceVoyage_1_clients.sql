1c1bd6251341d6c250e4666aed2701d28a940eb0
MDevoldere <mdevoldere@users.noreply.github.com>
2020-12-10 10:26:41 +0100

    MAJ

--- USAL37/TP_AgenceVoyage/TP_AgenceVoyage_1_clients.sql
+++ USAL37/TP_AgenceVoyage/TP_AgenceVoyage_1_clients.sql
@@ -2,7 +2,11 @@
 1) 	Créer la table "sales" (commerciaux) et ajouter 5 commerciaux
 
 2) 	Créer la table "clients" et ajouter 5 clients dans la table
-	Chaque client est associé à un commercila différent 
+	Chaque client est associé à un commercial différent 
     
 3) 	Sélectionnez tous les clients (nom du commercial associé inclus)
 */
+
+use usal37_agence;
+
+CREATE ....
